6ca83abdb03d17b56468aa58388350a55fce6041
Ismael Pastor <ispaluan@gmail.com>
2025-04-04 12:35:29 +0200

    Terminado

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 const countriesList = document.getElementById('countries-list');
 const url = 'https://restcountries.com/v3/all';
+const info = document.getElementById('info');
 
 const getCountries = async () => {
     try{
@@ -12,15 +13,43 @@ const getCountries = async () => {
         throw new Error(`Error: ${response.status}`)
     }
     const countries = await response.json()
-
-    
+    sortedCountries(countries)
     countriesList.innerHTML = countryArray(countries).join("")
+
+    const cards = document.querySelectorAll(".card")
+
+    cards.forEach((card, index) => {
+      card.addEventListener("click", () => {
+        const country = countries[index]
+        const { name: {common}, flags, capital, population, car: {side} } = country
+        const template = `
+          <div class="cardInfo">
+            <img src=${flags[1]} alt=${common}/>
+            <h2>${common}</h2>
+            <p><strong>Capital:</strong> ${capital}</p>
+            <p><strong>Población:</strong> ${population}</p>
+            <p><strong>Lado Conducción:</strong> ${side}</p>
+            <button onclick="closeInfo()">Cerrar</button>
+          </div>
+        `
+        info.innerHTML = template
+        info.classList.add("visible")
+
+      })
+
+    })
+
     }catch (err) {
         console.error(err)
     }
 }
 
 getCountries()
+
+const closeInfo = () => {
+    info.innerHTML = ""
+    info.classList.remove('visible')
+}
 
 const templateCard = (name, flag) => {
     const template =`
@@ -32,81 +61,22 @@ const templateCard = (name, flag) => {
     return template
 }
 
-
 const countryArray = (countries) => {
     const allCountries = countries.map(country => {
-        const {name: {common}, flags} = country
-        return templateCard(common,flags)
+        const {name: {common}, flags, capital, population, car: {side}} = country
+        return templateCard(common,flags, capital, population, side)
     })
     return allCountries
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*async function banderas() {
-    try {
-        const response = await fetch(url);
-        if(!response.ok) {
-            throw new Error ('No hemos podido cargar la página', response.status)
-        }
-        
-        const pais = await response.json();
-        const bandera = pais.filter((pais) => pais.name );
-        bandera.sort(function (a, b) {
-            if (a.name.official > b.name.official) {
-                return 1;
-            } 
-            if (a.name.official < b.name.official) {
-                return -1;
-            }
-            return 0;
-        });
-        bandera.forEach((pais) => {
-            const {side} = pais.car;
-            const infoBandera = {
-                ...pais,
-                car: `${side}`,
-            }
-            const {flag, capital, population, car} = infoBandera
-            countriesList.innerHTML +=`
-            <ul>
-              <li>
-                <img src="${pais.flags[1]}" alt="${pais.name.official}"/>
-                <h2>${pais.name.official}</h2>
-              </li>
-            </ul>`
-            countriesList.addEventListener('click', (event) => {
-                if (event.target.tagName === 'IMG') {
-                    window.open()
-                }
-            })
-        });
-        
-        } catch (error) {
-        console.log('Error al cargar', error)
-        } 
+ const sortedCountries = (countries) => {
+    countries.sort((a, b) => {
+        const nameA = a.name.common.toUpperCase()
+        const nameB = b.name.common.toUpperCase()
+        return nameA.localeCompare(nameB)
+    })
+ 
 }
 
-banderas()
 
-*/
+
